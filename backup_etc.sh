12157d5014b77f4f97743b47eafb29e094ffb2bf
Ben <ben.edu@safeguardsupport.com>
2025-11-17 10:44:12 +0100

    add backup script for /etc

---
+++ backup_etc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# تعریف مسیرها
+SOURCE="/etc"
+DEST="/backup"
+
+# ساخت نام فایل بکاپ با تاریخ و ساعت
+DATE=$(date +'%Y-%m-%d_%H-%M-%S')
+FILENAME="etc_backup_$DATE.tar.gz"
+
+# چک کردن وجود پوشه بکاپ
+if [ ! -d "$DEST" ]; then
+    mkdir -p "$DEST"
+fi
+
+# حذف بکاپ‌های قدیمی‌تر از 7 روز
+find "$DEST" -name "etc_backup_*.tar.gz" -type f -mtime +7 -exec rm {} \;
+
+# اجرای بکاپ با tar
+tar -czf "$DEST/$FILENAME" "$SOURCE"
+
+# بررسی موفقیت عملیات
+if [ $? -eq 0 ]; then
+    echo "Backup successful: $DEST/$FILENAME"
+else
+    echo "Backup failed!"
+    exit 1
+fi
